2e358c9f060312a39cbd3172e45c2203f3592938
Dzianis Talkachou <dtalkachou@ya.ru>
2019-12-17 14:33:05 +0300

    Fix bug with clear()/clear_all() when changing orientation

--- Calculator/app/src/main/java/com/dtalkachou/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/dtalkachou/calculator/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -53,12 +54,16 @@ public class MainActivity extends AppCompatActivity
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putParcelable("calculator", calculator);
+        savedInstanceState.putString("clearBtnText",
+                ((Button) findViewById(R.id.clear)).getText().toString());
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         calculator = savedInstanceState.getParcelable("calculator");
+        ((Button) findViewById(R.id.clear)).setText(savedInstanceState.getString("clearBtnText"));
+        // Initialize calculator on the display.
         onCalculatorButtonPressed();
     }
 
