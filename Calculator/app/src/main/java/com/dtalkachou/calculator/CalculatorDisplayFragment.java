ab8de24c05ccd3674cbb7a9337cfb6f23303908f
Dzianis Talkachou <dtalkachou@ya.ru>
2019-12-17 16:17:09 +0300

    Auto scroll historyStr to right

--- Calculator/app/src/main/java/com/dtalkachou/calculator/CalculatorDisplayFragment.java
+++ Calculator/app/src/main/java/com/dtalkachou/calculator/CalculatorDisplayFragment.java
@@ -2,21 +2,48 @@ package com.dtalkachou.calculator;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class CalculatorDisplayFragment extends Fragment {
+    private HorizontalScrollView horizontalScrollView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.calculator_display, container, false);
 
         initChangeMode(view);
+        initHistoryStr(view);
         return view;
+    }
+
+    private void initHistoryStr(View view) {
+        horizontalScrollView = view.findViewById(R.id.history_scroll_view);
+        ((TextView) view.findViewById(R.id.history_str)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                horizontalScrollView.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+            }
+        });
     }
 
     private void initChangeMode(View view) {
